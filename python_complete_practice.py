7ac31b2e0738187f909b320212b3952cf9a41625
Shahid Shaik <galibshahid2008@gmail.com>
2018-11-25 23:42:39 +0530

    Update python_complete_practice.py

--- python_complete_practice.py
+++ python_complete_practice.py
@@ -58,11 +58,12 @@ print(c)
 """ 2)Logical Operators
         == comparision
         != not equals to
-        >= less than or equals
-        <= greater than or equals
-        < is greater than
-        > is less than
+        <= less than or equals
+        >= greater than or equals
+        > is greater than
+        < is less than
         <> not equals to
+
 Note: For if else conditions syntax are mandatory
         if ():
             <tap space>
@@ -79,3 +80,27 @@ if a != b:
     print("both A="+ str(a)+ " and B="+ str(b) +" values are NOT same ")
 else:
     print("both A="+ str(a)+ " and B="+ str(b) +" values are same ")
+#-------- less than or equals------------#
+a=3
+b=4
+if a <= b:
+    print("A less than or equals B "+"A="+str(a)+" and B="+str(b))
+else:
+    print("A greate than or not equals B "+"A="+str(a)+" and B="+str(b))
+#-------- greater than or equals------------#
+a=7
+b=4
+if a >= b:
+    print("A greater than or equals B "+"A="+str(a)+" and B="+str(b))
+else:
+    print("A less than or not equals B "+"A="+str(a)+" and B="+str(b))
+#-------- greater than------------#
+if a > b:
+    print("A greater than B "+"A="+str(a)+" and B="+str(b))
+else:
+    print("A not greater than B "+"A="+str(a)+" and B="+str(b))
+#-------- less than------------#
+if a < b:
+    print("A less than B "+"A="+str(a)+" and B="+str(b))
+else:
+    print("A not less than B "+"A="+str(a)+" and B="+str(b))
